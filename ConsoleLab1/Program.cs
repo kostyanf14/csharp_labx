1e12a6ab1fe23302be76a7336448276ec6fd62b0
kostyanf14 <kostyanf14@K29-TEACHER>
2023-03-17 11:02:04 +0200

    Додали текст

--- ConsoleLab1/Program.cs
+++ ConsoleLab1/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace ConsoleLab1
 {
@@ -10,6 +7,7 @@ namespace ConsoleLab1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello");
         }
     }
 }
